0c2848f577de8152884e1e133615cb1529a92294
Guillaume Roboam <guillaume_roboam@hotmail.fr>
2025-09-21 23:11:29 +0200

    feat: add KTH education

--- src/components/homepage/education.jsx
+++ src/components/homepage/education.jsx
@@ -13,6 +13,25 @@ const Education = () => {
 					<div className="education-body">
 						<div className="education-item">
 							<img
+								src="./kth.png"
+								alt="kth"
+								className="education-image"
+							/>
+							<div className="education-info">
+								<div className="education-title">
+									KTH Royal Institute of Technology
+								</div>
+								<div className="education-subtitle">
+									Exchange semester in Computer Science at
+									Sweden's best CS engineering school
+								</div>
+							</div>
+							<div className="education-duration">
+								2025 - 2026
+							</div>
+						</div>
+						<div className="education-item">
+							<img
 								src="./ensimag.png"
 								alt="ensimag"
 								className="education-image"
@@ -20,7 +39,8 @@ const Education = () => {
 							<div className="education-info">
 								<div className="education-title">ENSIMAG</div>
 								<div className="education-subtitle">
-									Computer Science at France's best CS engineering school
+									Computer Science at France's best CS
+									engineering school
 								</div>
 							</div>
 							<div className="education-duration">
